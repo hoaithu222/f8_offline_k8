5c001f2558e3b7ce050b57067cb2dc26523de3c9
Thuu <you@example.com>
2024-05-22 19:02:31 +0700

    Nộp bài tập

--- homework/Day-17/js/ex02.js
+++ homework/Day-17/js/ex02.js
@@ -3,4 +3,4 @@ var b =  5;
 var c = 2;
 const sum = a + (a * c) + (Math.pow(b,a))/c;
 console.log(sum);
-document.write(`Biểu tính có giá trị = ${sum}`);
+document.write(`Biểu thức cần tính có giá trị = ${sum}`);
