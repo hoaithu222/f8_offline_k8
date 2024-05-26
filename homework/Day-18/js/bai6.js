e74b60a68f43eff9fd1feb6dbfb14cc5f2d0de63
Thuu <you@example.com>
2024-05-26 16:24:44 +0700

    Nộp bài tập

--- homework/Day-18/js/bai6.js
+++ homework/Day-18/js/bai6.js
@@ -1,6 +1,6 @@
 document.write('<div class="chessboard">');
-for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
+for (var i = 1; i<= 8; i++) {
+    for (let j = 1; j <= 8; j++) {
         let color = (i + j) % 2 === 0 ? 'white' : 'black';
         document.write('<div class="square ' + color + '"></div>');
     }
