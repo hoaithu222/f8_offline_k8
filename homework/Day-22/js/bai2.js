ab143734a3c143e0f6b84d759971849d16fea67f
Thuu <you@example.com>
2024-06-03 19:17:19 +0700

    Nộp bài tập ngày 22

--- homework/Day-22/js/bai2.js
+++ homework/Day-22/js/bai2.js
@@ -6,34 +6,35 @@ for (var i = 0; i < n; i++) {
     a[i] = new Array(m);
     for (var j = 0; j < m; j++) {
         var tmp = prompt(`Nhập giá trị hàng thứ ${i} và cột thứ ${j}: `);
-        a[i][j] = tmp;
-    }
-}
-for(var i = 0; i < n; i++){
-    for(var j = 0; j < m; j++){
-        if(typeof a[i][j] === 'string'){
-            document.write(a[i][j]+" ");
+        if (!isNaN(tmp) && tmp.trim() !== '') {
+            a[i][j] = Number(tmp);
+        } else if (tmp.toLowerCase() === 'true' || tmp.toLowerCase() === 'false') {
+            a[i][j] = (tmp.toLowerCase() === 'true');
+        } else {
+            a[i][j] = tmp;
         }
-    
     }
-    document.write("<br>");
-}
-for(var i = 0; i < n; i++){
-    for(var j = 0; j < m; j++){
-        if(typeof a[i][j] === 'number'){
-            document.write(a[i][j]+" ");
-        }
-    
-    }
-    document.write("<br>");
-}
-for(var i = 0; i < n; i++){
-    for(var j = 0; j < m; j++){
-        if(typeof a[i][j] === 'boolean'){
-            document.write(a[i][j]+" ");
-        }
-    
-    }
-    document.write("<br>");
 }
 
+var stringValues = [];
+var numberValues = [];
+var booleanValues = [];
+
+for (var i = 0; i < n; i++) {
+    for (var j = 0; j < m; j++) {
+        if (typeof a[i][j] === 'string') {
+            stringValues.push(a[i][j]);
+        } else if (typeof a[i][j] === 'number') {
+            numberValues.push(a[i][j]);
+        } else if (typeof a[i][j] === 'boolean') {
+            booleanValues.push(a[i][j]);
+        }
+    }
+}
+
+stringValues.forEach(value => document.write(value + " "));
+document.write("<br>");
+numberValues.forEach(value => document.write(value + " "));
+document.write("<br>");
+booleanValues.forEach(value => document.write(value + " "));
+document.write("<br>");
